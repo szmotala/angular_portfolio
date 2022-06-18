060c75e9979c4e1ebc89eee4b6e548583d1b9fbb
Szymon Motała <szymon.motala95@gmail.com>
2022-06-18 19:41:53 +0200

    favicon + titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
 
 const routes: Routes = [
-  { path: '',  component: HomePageComponent, data: { page: 'HomePage' }},
-  { path: 'about', component: AboutPageComponent, data: { page: 'AboutPage' }},
-  { path: 'contact', component: ContactPageComponent, data: { page: 'ContactPage' }},
-  { path: 'blog_app', component: ProjectPageComponent, data: { page: 'BlogPage', type: 'blog' }},
-  { path: 'photography_portfolio', component: ProjectPageComponent, data: { page: 'PhotoPage', type: 'photography' }},
+  { path: '',  component: HomePageComponent, title: "Szymon Motała Portfolio",  data: { page: 'HomePage' }},
+  { path: 'about', component: AboutPageComponent, title: "About me", data: { page: 'AboutPage' }},
+  { path: 'contact', component: ContactPageComponent, title: "Contact", data: { page: 'ContactPage' }},
+  { path: 'blog_app', component: ProjectPageComponent, title: "Blog app project", data: { page: 'BlogPage', type: 'blog' }},
+  { path: 'photography_portfolio', component: ProjectPageComponent, title: "Photography project", data: { page: 'PhotoPage', type: 'photography' }},
 ];
 
 @NgModule({
